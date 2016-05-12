00f410d75734bb310c3b6c63571bc092786321bb
Tom Milligan <tmilligan@skytap.com>
2016-05-12 11:08:18 -0500

    Remove release 2.03 artifacts

--- cleanup-plugin.sh
+++ cleanup-plugin.sh
@@ -1,4 +1,4 @@
 /usr/local/apache-maven-3.0.5/bin/mvn -DskipTests clean
 rm -f release.properties pom.xml.releaseBackup
-git tag -d skytap-2.03
-git push --delete origin skytap-2.03
+# git tag -d skytap-2.03
+# git push --delete origin skytap-2.03
