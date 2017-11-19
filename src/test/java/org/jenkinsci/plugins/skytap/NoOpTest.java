164df85fdf951f9f612bba343bb494b4d471a0ab
Colin Henry <jchenry@skytap.com>
2017-11-19 14:54:11 -0800

    added simple test get build pipeline to clear, will add real tests later

---
+++ src/test/java/org/jenkinsci/plugins/skytap/NoOpTest.java
@@ -0,0 +1,16 @@
+ package hello;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NoOpTest {
+
+
+	@Test
+	public void testTrue() {
+		// no op
+	}
+
+}
